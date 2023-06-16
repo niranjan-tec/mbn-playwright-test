c8eeeb1e1a6f00b03eee73cc5a1693bab3f36b2f
Niranjan <niranjankumar2252@gmail.com>
2023-06-16 18:10:16 +0530

    add-changes

--- pages/internet-access-form.ts
+++ pages/internet-access-form.ts
@@ -9,17 +9,24 @@ constructor(page:Page){
     this.page=page;
 }
 
-formSearchBox = () => this.page.locator('#Searchforms');
-selectInternetAccessForm = () => this.page.locator('#ui-id-36');
-selectInter =() => this.page.getByAltText('Internet Access Form | IA');
+selectDepartment = () => this.page.locator("#inputGroupSelect02");
+selectFormName = () => this.page.locator('#inputGroupSelect01');
+searchButton = () => this.page.locator('[id="searchBtn"]');
+collapseTab = () => this.page.locator('[id=collapseOne]');
+selectDesignation = () => this.page.locator('[name="ddEmpDesignation"]');
+selectLocation =() => this.page.locator('[name="ddEmpLocation"]');
+selectRequestSubmissionTypes = () => this.page.locator('[id="drpRequestSubmissionFor"]');
 
 
 
 async clickSearchBox(){
-    await this.formSearchBox().fill("Internet Access Form");
-
-    await this.selectInter().click();
-
+    await this.selectDepartment().selectOption('IT');
+    await this.selectFormName().selectOption('IA');
+    await this.searchButton().click();
+    await this.collapseTab().click();
+    await this.selectDesignation().selectOption('Managing Director');
+    await this.selectLocation().selectOption('SAVW - Pune Chakan Plant');
+    await this.selectRequestSubmissionTypes().selectOption('Self');
 }
 
 }
