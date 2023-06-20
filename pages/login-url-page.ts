87f0828a9f8c2506f0fcb68353f162448341ec79
Niranjan <120706073+niranjan-tec@users.noreply.github.com>
2023-06-20 09:46:46 +0530

    Update login-url-page.ts

--- pages/login-url-page.ts
+++ pages/login-url-page.ts
@@ -11,7 +11,7 @@ export class LoginUrlPage{
 
     async eFormLoginUrl(){
         //await this.page.goto("http://win-6cgsdmg51od:8500/");
-        await this.page.goto("https://playwright.dev/docs/ci-intro")
+        await this.page.goto("http://win-6cgsdmg51od:50000/sites/MPR/SitePages/MyDashboard.aspx/");
     }
     
 }
